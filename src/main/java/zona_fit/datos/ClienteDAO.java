0de433d30722993e7b5086181fc6623600b4781a
Cnon <cnon@hotmail.es>
2025-10-09 11:36:43 +0200

    ClienteDAO hecho

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -2,13 +2,39 @@ package zona_fit.datos;
 
 import zona_fit.dominio.Cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import static zona_fit.conexion.Conexion.getConexion;
 
 public class ClienteDAO implements IClienteDAO {
 
 
     @Override
     public List<Cliente> listarClientes() {
+
+        List <Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        String sql = "SELECT * FROM cliente ORDER by id";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        }catch (Exception e){
+            System.out.println("error al listar clientes" + e.getMessage());
+        }
         return null;
     }
 
